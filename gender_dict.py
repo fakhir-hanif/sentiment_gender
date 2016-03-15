83e50d67b5279b24f6619ca7e2b81c812aa2b28c
Fakhir Hanif <fakhir_hanif@hotmail.com>
2016-03-15 19:18:46 +0500

    Update gender_dict.py

--- gender_dict.py
+++ gender_dict.py
@@ -1,4 +1,6 @@
 gender = {
   'fakhir': 'male', 'Amina': 'female', 'telenor': 'undefined', 'shahid': 'male', 'talkshalk': 'undefined', 'Ikram': 'male',
-  'Pakistan': 'undefined', 'Raja': 'male', 'Pome': 'male'
+  'Pakistan': 'undefined', 'Raja': 'male', 'Pome': 'male', 'djuice': 'undefined', 'Zikriya': 'male', 'djuice': 'undefined',
+  'Easypesa': 'undefined', 'qaswar': 'male', 'shahjehan': 'male', 'shoaib': 'male', 'rana': 'male', 'abrar': 'male', 'allahdad': 'male',
+  
 }
