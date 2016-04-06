a58a3e0185aa8cd4fe45ac4cef2a52e3fa17cf2b
Fakhir Hanif <fakhir_hanif@hotmail.com>
2016-04-06 18:00:55 +0500

    Update gender_dict.py

--- gender_dict.py
+++ gender_dict.py
@@ -6,5 +6,6 @@ gender = {
   'shaam': 'male', 'bigra': 'male', 'arsam': 'male', 'engr': 'male', 'inzamam': 'male', 'AR': 'male', 'panjatan': 'undefined', 
   'panjtan': 'undefined', 'kami': 'male', 'jinsar': 'male', 'ghamgin': 'male', 'ghamgeen': 'male', 'joni': 'male', 'janzeb': 'male', 
   'sheikhzada': 'male', 'star': 'male', 'ipl': 'undefined', 'ary': 'undefined', 'tt': 'undefined', 'f': 'undefined', 'nge': 'undefined',
-  'jutt': 'male', 'suhana': 'male', 'snl': 'undefined', 'haroonleo': 'male', 'zulfqar': 'male', 
+  'jutt': 'male', 'suhana': 'male', 'snl': 'undefined', 'haroonleo': 'male', 'zulfqar': 'male', 'telecom': 'undefined',
+  'marketing': 'undefined', 'pinzoo': 'undefined', 'free': 'unidefined', 
 }
